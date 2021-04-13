eddf3281b89aa547870a00800477b48d01300703
JackieXu <343141195@qq.com>
2021-04-13 16:54:15 +0800

    swagger

--- src/main/java/com/jackie/stockbean/fund/dto/request/FundAddReqDTO.java
+++ src/main/java/com/jackie/stockbean/fund/dto/request/FundAddReqDTO.java
@@ -1,8 +1,11 @@
 package com.jackie.stockbean.fund.dto.request;
 
 import com.jackie.stockbean.fund.vo.DistributeVo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -13,9 +16,14 @@ import java.util.List;
  * @date 2021/4/7
  */
 @Data
-public class FundAddReqDTO {
+@ApiModel(value="基金添加参数",description="基金添加参数")
+public class FundAddReqDTO implements Serializable {
+    @ApiModelProperty(value="基金代码", name="code", required = true)
     private String code;
+    @ApiModelProperty(value="基金名称", name="name", required = true)
     private String name;
+    @ApiModelProperty(value="基金投资占比", name="distributeList", required = true)
     private List<DistributeVo> distributeList;
+    @ApiModelProperty(value="基金股票投资占比", name="stockDistributeList", required = true)
     private List<DistributeVo> stockDistributeList;
 }
--- src/main/java/com/jackie/stockbean/fund/vo/DistributeVo.java
+++ src/main/java/com/jackie/stockbean/fund/vo/DistributeVo.java
@@ -1,5 +1,7 @@
 package com.jackie.stockbean.fund.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -10,8 +12,12 @@ import lombok.Data;
  * @date 2021/4/6
  */
 @Data
+@ApiModel(value="占比内容",description="占比内容")
 public class DistributeVo {
+    @ApiModelProperty(value="代码", name="code")
     private String code;
+    @ApiModelProperty(value="名称", name="name", required = true)
     private String name;
+    @ApiModelProperty(value="占比", name="proportion", required = true)
     private Double proportion;
 }
